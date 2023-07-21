ed2d8cc7f1b6840f278719bfd80cf179375913c8
maver1ck1209 <giridharsunil@gmail.com>
2023-07-21 20:49:31 +0530

    django-m

--- demo4.py
+++ demo4.py
@@ -1,7 +1,7 @@
 import replicate
 import os
 os.environ['REPLICATE_API_TOKEN']='r8_6N0lkVMsdoSTVULfOVZvKbRTNuyiLKv3aW4mJ'
-PROMPT_FOR_MODEL='''
+DIET_PROMPT_FOR_MODEL='''
 Generate a list of Foods that can be prepared using the following Ingredients :
 {ingd}
 
@@ -20,5 +20,5 @@ PROMPT_FOR_MODEL.format({'age':age,'city':city,'topics':topics})
 output = replicate.run(
     "a16z-infra/llama13b-v2-chat:5c785d117c5bcdd1928d5a9acb1ffa6272d6cf13fcb722e90886a0196633f9d3",
     input={"prompt": 
-    PROMPT_FOR_MODEL,'max_length':2000,"temperature":0.75}
+    DIET_PROMPT_FOR_MODEL,'max_length':2000,"temperature":0.75}
 )
--- email/email_Api/api/views.py
+++ email/email_Api/api/views.py
@@ -1,3 +1,82 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import get_template
+from django.views.decorators.csrf import csrf_exempt
+import pyrebase
+import json
 
-# Create your views here.
+import replicate
+import os
+os.environ['REPLICATE_API_TOKEN']='r8_3qVVdwlTNa1h7WlGgPQ3ClRXEEzwW2528PQCt'
+PROMPT_FOR_MODEL='''
+You are to generate a mail to market a health app that is personalized to the user's need.
+Create mail to cater to a user with only the following data and do not add any other data:
+Age:{age}
+City:{city}
+Interested Topics: {topics}
+Do not make your own conclusions, create only necessary details.
+You only have to generate the content of the mail.
+'''
+DIET_PROMPT_FOR_MODEL='''
+Generate a list of Foods that can be prepared using the following Ingredients :
+{ingds}
+
+Keep in mind the user has the following Symptoms : 
+{Symptoms}
+
+And the following Conditions : 
+{cnd}
+'''
+
+
+
+config={
+  'apiKey': "AIzaSyB-XZ68XuIyP4l-ghWgV4Y-dIg4Zw_LVQM",
+  'authDomain': "hackrx-pre.firebaseapp.com",
+  'databaseURL': "https://hackrx-pre-default-rtdb.asia-southeast1.firebasedatabase.app",
+  'projectId': "hackrx-pre",
+  'storageBucket': "hackrx-pre.appspot.com",
+  'messagingSenderId': "33665546378",
+  'appId': "1:33665546378:web:3079bc47ae1b5ce62f1573"
+}
+
+db=pyrebase.initialize_app(config)
+store=db.storage()
+
+rl_db=db.database()
+
+def email(request):
+    if request.method == 'GET':
+        htmly = get_template('Email.html')
+        subject, from_email, to = 'welcome', 'your_email@gmail.com', email
+        d = {'content': 'abc'}
+        html_content = htmly.render(d)
+        msg = EmailMultiAlternatives(subject, html_content, from_email, [to])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
+        return {
+            'success': 1
+        }
+
+@csrf_exempt
+def diet(request):
+    if request.method=='POST':
+        data=json.loads(request.body)
+        if 'text' in data:
+            ingds = data['text']
+        else:
+            ingds = 'random ingredients'
+        symds='fever'
+        cnds='Diabetes'
+        prompt=DIET_PROMPT_FOR_MODEL.format(Symptoms=symds,ingds=ingds, cnd = cnds)
+
+        output = replicate.run(
+            "a16z-infra/llama13b-v2-chat:5c785d117c5bcdd1928d5a9acb1ffa6272d6cf13fcb722e90886a0196633f9d3",
+            input={"prompt": 
+            prompt,'max_length':2000,"temperature":0.75}
+        )
+        ot=''
+        for i in output:
+            ot+=i
+        return JsonResponse({'output':ot})
